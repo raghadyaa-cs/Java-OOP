dbe798bb86b2a6e3a98ebba23388623f88434444
Raghad Yaser AbuAtwan 🪼⋆.ೃ࿔*:･ <raghadyaserabuatwan@gmail.com>
2025-11-15 16:29:28 +0300

    week5.lec1
    creating methods

---
+++ Person.java
@@ -0,0 +1,16 @@
+package edu.zuj.oop.week5.lecture1;
+public class Person {
+    //Instance variables
+    String  name;
+    int age;
+    
+    public void talk(){
+        System.out.println("Hello, my name is "+name);
+    }
+    
+    public void walk(){
+        System.out.println("I am walking....");
+    }
+    
+    
+}
