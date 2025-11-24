8b03dc77d4d5040ebb2a78f02fd9c285c8ffe4e0
Raghad Yaser AbuAtwan 🪼⋆.ೃ࿔*:･ <raghadyaserabuatwan@gmail.com>
2025-11-24 20:56:56 +0300

    Edit PersonTest

--- week5.lec2.java
+++ week5.lec2.java
@@ -3,14 +3,15 @@ package edu.zuj.oop.week5.lecture1;
 public class PersonTest {
     public static void main(String[] args) {
         Person p1=new Person();
-        p1.name="Ata";
+        p1.name="Ali";
         p1.age=15;
         
         Person p2=new Person();
         p2.name="Kamal";
         p2.age=20;
         
-        p1.talk();//Hello, my name is Ata
+        p1.talk();//Hello, my name is Ali
         p2.talk();//Hello, my name is Kamal
     }
 }
+
