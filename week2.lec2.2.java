cec079794731179949906e69d49bacac6e92145e
Raghad Yaser AbuAtwan 🪼⋆.ೃ࿔*:･ <raghadyaserabuatwan@gmail.com>
2025-12-18 11:02:40 +0300

    Add Test2 class for user name input and greeting

---
+++ week2.lec2.2.java
@@ -0,0 +1,13 @@
+package edu.zuj.oop.week2.lecture2;
+
+import java.util.Scanner;
+
+public class Test2 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name=s.next();
+        System.out.println("Hello, "+name);
+        
+    }
+}
