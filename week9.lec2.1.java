c0b3ee7a4ce148ad590f93d3fd41c0376be57c8f
Raghad Yaser AbuAtwan 🪼⋆.ೃ࿔*:･ <raghadyaserabuatwan@gmail.com>
2025-12-18 11:33:32 +0300

    Create class A with methods m1, m2, and m3

---
+++ week9.lec2.1.java
@@ -0,0 +1,17 @@
+package edu.zuj.oop.week9.lecture2;
+
+public class A {
+
+    public void m1() {
+        System.out.println("m1 from A");
+    }
+
+    public void m2() {
+        System.out.println("m2 from A");
+    }
+
+    public void m3() {
+        System.out.println("m3 from A");
+    }
+
+}
