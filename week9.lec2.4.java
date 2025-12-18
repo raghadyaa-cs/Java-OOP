3f5b5d406099032c0fab4e77cb94081d68a7e5cf
Raghad Yaser AbuAtwan 🪼⋆.ೃ࿔*:･ <raghadyaserabuatwan@gmail.com>
2025-12-18 11:41:17 +0300

    Add Car class with basic functionality

---
+++ week9.lec2.4.java
@@ -0,0 +1,22 @@
+package raghadyaaoop;
+
+
+public class Car {
+
+    public void turnOn() {
+        System.out.println("start engine");
+    }
+
+    public void turnOff() {
+        System.out.println("stop engine");
+    }
+
+    public void move() {
+        System.out.println("move forward");
+    }
+
+    public void stop() {
+        System.out.println("stop moving");
+    }
+
+}
