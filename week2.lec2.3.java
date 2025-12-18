4df332207098051a5088dc962dc0ea85fffe0eb0
Raghad Yaser AbuAtwan 🪼⋆.ೃ࿔*:･ <raghadyaserabuatwan@gmail.com>
2025-12-18 11:03:37 +0300

    Create week2.lec2.3.java

---
+++ week2.lec2.3.java
@@ -0,0 +1,14 @@
+package edu.zuj.oop.week2.lecture2;
+
+import java.util.Scanner;
+
+public class Test3 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in); 
+        System.out.print("Enter n1:");
+        int n1=s.nextInt();
+        System.out.print("Enter n2:");
+        int n2=s.nextInt();
+        System.out.println("Reslut ="+(n1+n2));
+    }
+}
