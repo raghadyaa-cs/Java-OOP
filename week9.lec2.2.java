a4dc37155a9b4cd5a766e31c1c2bd70372b0a908
Raghad Yaser AbuAtwan 🪼⋆.ೃ࿔*:･ <raghadyaserabuatwan@gmail.com>
2025-12-18 11:34:11 +0300

    Add class B that extends A with methods m3 and m4

---
+++ week9.lec2.2.java
@@ -0,0 +1,14 @@
+package edu.zuj.oop.week9.lecture2;
+
+public class B extends A{
+
+    @Override
+    public void m3() {
+        System.out.println("m3 from B");
+    }
+    
+    
+    public void m4(){
+        System.out.println("m4 from B");
+    }
+}
