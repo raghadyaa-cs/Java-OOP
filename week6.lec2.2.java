bb938975351aa396252444e93eeee5296b882f47
Raghad Yaser AbuAtwan 🪼⋆.ೃ࿔*:･ <raghadyaserabuatwan@gmail.com>
2025-12-18 11:27:05 +0300

    Add CircleTest class to test Circle functionality

---
+++ week6.lec2.2.java
@@ -0,0 +1,13 @@
+package edu.zuj.oop.week6.lecture2;
+
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle c1=new Circle(10);
+        Circle c2=new Circle(5);
+        
+        System.out.println(c1);
+        System.out.println(c2);
+        
+        
+    }
+}
